1e69de1eca14ae36b10d933547bd634776b2b0f4
dana.nurse <dana.nurse@outlook.com>
2015-01-02 22:58:51 +0000

    Initial Commit

---
+++ SignalRService/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SignalRService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+//                Timer t = new Timer(TimerTick, null, 0, 1000);
+                Console.ReadLine();
+            }
+        }
+
+        private static void TimerTick(object state)
+        {
+            string message = DateTime.Now.ToLongTimeString();
+            Console.WriteLine(message);
+            ServiceHub.SendServerMessage("Server", message);
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    public class ServiceHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+
+        internal static void SendServerMessage(string name, string message)
+        {
+            IHubContext con = GlobalHost.ConnectionManager.GetHubContext<ServiceHub>();
+            con.Clients.All.addMessage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            string handle = Context.ConnectionId;
+            Clients.All.addMessage(handle, "connected");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string handle = Context.ConnectionId;
+            Clients.All.addMessage(handle, "disconnected");
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Handle { get; set; }
+        public IHubProxy HubProxy { get; set; }
+        private const string URI = "http://localhost:8080/signalr";
+        public HubConnection Connection { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendClick(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", Handle, TbMessage.Text);
+            TbMessage.Text = string.Empty;
+            TbMessage.Focus();
+        }
+
+        private void ConnectionClosed()
+        {
+            var dp = Application.Current.Dispatcher;
+            dp.Invoke(() => ChatPanel.Visibility = Visibility.Collapsed);
+            dp.Invoke(() => BtnSend.IsEnabled = false);
+            dp.Invoke(() => StatLabel.Content = string.Format("Disconnected from {0}", URI));
+            dp.Invoke(() => SignInPanel.Visibility = Visibility.Visible);
+        }
+
+        private async void ConnectAsync()
+        {
+            Connection = new HubConnection(URI);
+            Connection.Closed += ConnectionClosed;
+            HubProxy = Connection.CreateHubProxy("ServiceHub");
+
+            HubProxy.On<string, string>("AddMessage", (name, message) =>
+                this.Dispatcher.Invoke(() =>
+                    RtConsole.AppendText(string.Format("{0}: {1}\r", name, message))));
+
+            try
+            {
+                await Connection.Start();
+            }
+            catch (Exception)
+            {
+                StatLabel.Content = "Connection Failed";
+                return;
+            }
+
+            SignInPanel.Visibility = Visibility.Collapsed;
+            ChatPanel.Visibility = Visibility.Visible;
+            BtnSend.IsEnabled = true;
+            TbMessage.Focus();
+            RtConsole.AppendText("Conected to " + URI + "\r");
+        }
+
+        private void SignInClick(object sender, RoutedEventArgs e)
+        {
+            Handle = tbUser.Text;
+
+            if (!string.IsNullOrEmpty(Handle))
+            {
+                StatLabel.Visibility = Visibility.Visible;
+                StatLabel.Content = string.Format("Connecting to {0} ...", URI);
+                ConnectAsync();
+            }
+        }
+
+        private void ClientClosing(object sender, CancelEventArgs e)
+        {
+            if (Connection != null)
+            {
+                Connection.Stop();
+                Connection.Dispose();
+            }
+        }
+    }
+}
